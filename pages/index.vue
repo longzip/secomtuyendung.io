b74fd3b401fa1d377909ad60f3d73a03c6950c74
Lo Van Long <longlv@v-auto.com.vn>
2021-05-28 09:10:57 +0700

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
+  <!-- This example requires Tailwind CSS v2.0+ -->
   <div class="relative bg-white overflow-hidden">
     <div class="max-w-7xl mx-auto">
       <div
@@ -37,7 +37,7 @@
           <polygon points="50,0 100,0 50,100 0,100" />
         </svg>
 
-        <Popover>
+        <div>
           <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
             <nav
               class="
@@ -57,12 +57,14 @@
                   <a href="#">
                     <span class="sr-only">Workflow</span>
                     <img
-                      class="h-8 w-auto sm:h-10"
-                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                      class="h-16 w-auto sm:h-20"
+                      src="/images/logo.svg"
+                      alt="Công ty bảo vệ An Thịnh Phát tuyển dụng"
                     />
                   </a>
                   <div class="-mr-2 flex items-center md:hidden">
-                    <PopoverButton
+                    <button
+                      type="button"
                       class="
                         bg-white
                         rounded-md
@@ -76,10 +78,27 @@
                         focus:outline-none
                         focus:ring-2 focus:ring-inset focus:ring-indigo-500
                       "
+                      aria-expanded="false"
+                      @click="open = !open"
                     >
                       <span class="sr-only">Open main menu</span>
-                      <MenuIcon class="h-6 w-6" aria-hidden="true" />
-                    </PopoverButton>
+                      <!-- Heroicon name: outline/menu -->
+                      <svg
+                        class="h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M4 6h16M4 12h16M4 18h16"
+                        />
+                      </svg>
+                    </button>
                   </div>
                 </div>
               </div>
@@ -91,113 +110,132 @@
                   class="font-medium text-gray-500 hover:text-gray-900"
                   >{{ item.name }}</a
                 >
+
                 <a
                   href="#"
                   class="font-medium text-indigo-600 hover:text-indigo-500"
-                  >Log in</a
+                  >Đăng nhập</a
                 >
               </div>
             </nav>
           </div>
 
-          <transition
-            enter-active-class="duration-150 ease-out"
-            enter-from-class="opacity-0 scale-95"
-            enter-to-class="opacity-100 scale-100"
-            leave-active-class="duration-100 ease-in"
-            leave-from-class="opacity-100 scale-100"
-            leave-to-class="opacity-0 scale-95"
+          <!--
+          Mobile menu, show/hide based on menu open state.
+
+          Entering: "duration-150 ease-out"
+            From: "opacity-0 scale-95"
+            To: "opacity-100 scale-100"
+          Leaving: "duration-100 ease-in"
+            From: "opacity-100 scale-100"
+            To: "opacity-0 scale-95"
+        -->
+          <div
+            v-show="open"
+            class="
+              absolute
+              top-0
+              inset-x-0
+              p-2
+              transition
+              transform
+              origin-top-right
+              md:hidden
+            "
           >
-            <PopoverPanel
-              focus
+            <div
               class="
-                absolute
-                top-0
-                inset-x-0
-                p-2
-                transition
-                transform
-                origin-top-right
-                md:hidden
+                rounded-lg
+                shadow-md
+                bg-white
+                ring-1 ring-black ring-opacity-5
+                overflow-hidden
               "
             >
-              <div
-                class="
-                  rounded-lg
-                  shadow-md
-                  bg-white
-                  ring-1 ring-black ring-opacity-5
-                  overflow-hidden
-                "
-              >
-                <div class="px-5 pt-4 flex items-center justify-between">
-                  <div>
-                    <img
-                      class="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                      alt=""
-                    />
-                  </div>
-                  <div class="-mr-2">
-                    <PopoverButton
-                      class="
-                        bg-white
-                        rounded-md
-                        p-2
-                        inline-flex
-                        items-center
-                        justify-center
-                        text-gray-400
-                        hover:text-gray-500
-                        hover:bg-gray-100
-                        focus:outline-none
-                        focus:ring-2 focus:ring-inset focus:ring-indigo-500
-                      "
-                    >
-                      <span class="sr-only">Close main menu</span>
-                      <XIcon class="h-6 w-6" aria-hidden="true" />
-                    </PopoverButton>
-                  </div>
+              <div class="px-5 pt-4 flex items-center justify-between">
+                <div>
+                  <img
+                    class="h-20 w-auto"
+                    src="/images/logo.svg"
+                    alt="Công ty bảo vệ An Thịnh Phát Tuyển dụng"
+                  />
                 </div>
-                <div class="px-2 pt-2 pb-3 space-y-1">
-                  <a
-                    v-for="item in navigation"
-                    :key="item.name"
-                    :href="item.href"
+                <div class="-mr-2">
+                  <button
+                    type="button"
                     class="
-                      block
-                      px-3
-                      py-2
+                      bg-white
                       rounded-md
-                      text-base
-                      font-medium
-                      text-gray-700
-                      hover:text-gray-900
-                      hover:bg-gray-50
+                      p-2
+                      inline-flex
+                      items-center
+                      justify-center
+                      text-gray-400
+                      hover:text-gray-500
+                      hover:bg-gray-100
+                      focus:outline-none
+                      focus:ring-2 focus:ring-inset focus:ring-indigo-500
                     "
-                    >{{ item.name }}</a
+                    @click="open = !open"
                   >
+                    <span class="sr-only">Close main menu</span>
+                    <!-- Heroicon name: outline/x -->
+                    <svg
+                      class="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
                 </div>
+              </div>
+              <div class="px-2 pt-2 pb-3 space-y-1">
                 <a
-                  href="#"
+                  v-for="item in navigation"
+                  :key="item.name"
+                  :href="item.href"
                   class="
                     block
-                    w-full
-                    px-5
-                    py-3
-                    text-center
+                    px-3
+                    py-2
+                    rounded-md
+                    text-base
                     font-medium
-                    text-indigo-600
-                    bg-gray-50
-                    hover:bg-gray-100
+                    text-gray-700
+                    hover:text-gray-900
+                    hover:bg-gray-50
                   "
+                  >{{ item.name }}</a
                 >
-                  Log in
-                </a>
               </div>
-            </PopoverPanel>
-          </transition>
-        </Popover>
+              <a
+                href="#"
+                class="
+                  block
+                  w-full
+                  px-5
+                  py-3
+                  text-center
+                  font-medium
+                  text-indigo-600
+                  bg-gray-50
+                  hover:bg-gray-100
+                "
+              >
+                Đăng nhập
+              </a>
+            </div>
+          </div>
+        </div>
 
         <main
           class="
@@ -224,10 +262,9 @@
                 md:text-6xl
               "
             >
-              <span class="block xl:inline">Data to enrich your</span>
-              {{ ' ' }}
+              <span class="block xl:inline">Bạn muốn xin làm</span>
               <span class="block text-indigo-600 xl:inline"
-                >online business</span
+                >nhân viên bảo vệ</span
               >
             </h1>
             <p
@@ -243,9 +280,8 @@
                 lg:mx-0
               "
             >
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
+              Chúng tôi hoan nghênh đơn ứng tuyển của tất cả các ứng viên đủ
+              tiêu chuẩn, kể cả người thuộc nhóm thiểu số và người khuyết tật.
             </p>
             <div
               class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start"
@@ -272,7 +308,7 @@
                     md:px-10
                   "
                 >
-                  Get started
+                  Nộp hồ sơ
                 </a>
               </div>
               <div class="mt-3 sm:mt-0 sm:ml-3">
@@ -297,7 +333,7 @@
                     md:px-10
                   "
                 >
-                  Live demo
+                  Xem thông báo
                 </a>
               </div>
             </div>
@@ -308,7 +344,7 @@
     <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
       <img
         class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-        src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+        src="https://vn.vieclambaove.ga/wordpress/wp-content/uploads/2021/05/bao-ve-toa-nha-thay-the-1536x1109.jpg"
         alt=""
       />
     </div>
@@ -316,26 +352,15 @@
 </template>
 
 <script>
-import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue'
-import { MenuIcon, XIcon } from '@heroicons/vue/outline'
-
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Tuyển dụng', href: '#' },
+  { name: 'Hướng đẫn', href: '#' },
+  { name: 'Liên hệ', href: '#' },
 ]
-
 export default {
-  components: {
-    Popover,
-    PopoverButton,
-    PopoverPanel,
-    MenuIcon,
-    XIcon,
-  },
-  setup() {
+  asyncData() {
     return {
+      open: false,
       navigation,
     }
   },
